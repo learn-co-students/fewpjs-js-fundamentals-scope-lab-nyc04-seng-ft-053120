fd7711534c063171269037b26523fb063b1062d9
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-08-04 10:06:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-animal = ""
+let animal = "cat"
 
 function myAnimal() {
+  let animal = "dog"
   return animal
 }
 
@@ -12,8 +13,14 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
-  // Feel free to move things around!
-  two = 2
 }
+
+
+// 3) Fix the Scope add2(n) adds two to n:
+// ReferenceError: two is not defined
+//  at Window.add2 (file://index.html:17:14)
+//  at Context.<anonymous> (test/index-test.js:21:21)
+
